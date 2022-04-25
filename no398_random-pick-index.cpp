8fc2e64bcfc21c43a51afa920c79b031cf71ff5a
OmegaWei1984 <omegawei1984@gmail.com>
2022-04-25 13:34:07 +0800

    add cleaner code in no398_random-pick-index.cpp

--- no398_random-pick-index.cpp
+++ no398_random-pick-index.cpp
@@ -1,13 +1,13 @@
 #include <vector>
-#include <map>
 #include <iostream>
 #include <random>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    map<int, vector<int>> group;
+    unordered_map<int, vector<int>> group;
 
     Solution(vector<int>& nums) {
         for(int i = 0; i < nums.size(); ++i) {
@@ -26,10 +26,30 @@ public:
     }
 };
 
+class Solution1 {
+    unordered_map<int, vector<int>> pos;
+public:
+    Solution1(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            pos[nums[i]].push_back(i);
+        }
+    }
+    
+    int pick(int target) {
+        auto& indices = pos[target];
+        return indices[rand() % indices.size()];
+    }
+};
+
 int main(void) {
     vector<int> nums = {1,2,3,3,3};
     Solution* solution = new Solution(nums);
     int ans = solution->pick(3);
     ans = solution->pick(3);
+
+    Solution1 *solution1 = new Solution1(nums);
+    int ans1 = solution1->pick(3);
+    ans1 = solution1->pick(3);
+
     return 0;
 }
