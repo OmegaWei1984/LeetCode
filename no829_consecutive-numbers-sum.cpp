002086a3d378126d5c9e0c1857e72baf1ff27d26
OmegaWei1984 <omegawei1984@gmail.com>
2022-06-03 13:36:34 +0800

    add no829_consecutive-numbers-sum.cpp

---
+++ no829_consecutive-numbers-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int consecutiveNumbersSum(int n) {
+        int ans = 0;
+        for (int i = 1; i * (i + 1) <= 2 * n; ++i) {
+            if (
+                i % 2 == 0 ?
+                n % i != 0 && 2 * n * i == 0 :
+                n % i == 0
+            ) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(void) {
+    int n = 9;
+    Solution solution;
+    int ans = solution.consecutiveNumbersSum(n);
+    return 0;
+}
