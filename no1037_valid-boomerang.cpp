58250eae9b7a38bd65ccbd9bcfa67c3d2a0b3ee4
OmegaWei1984 <omegawei1984@gmail.com>
2022-06-08 22:12:40 +0800

    add no1037_valid-boomerang.cpp

---
+++ no1037_valid-boomerang.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        vector<int> v1 = {points[1][0] - points[0][0], points[1][1] - points[0][1]};
+        vector<int> v2 = {points[2][0] - points[0][0], points[2][1] - points[0][1]};
+        return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+    }
+};
+
+int main(void) {
+    vector<vector<int>> points = {{1, 1}, {2, 3}, {3, 2}};
+    Solution solution;
+    bool ans = solution.isBoomerang(points);
+    return 0;
+}
